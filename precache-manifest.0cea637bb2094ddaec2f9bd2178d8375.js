ba5993d493c2fae4f9e8beaf1ac5b72d5c7690d0
dev <dev@mail.com>
2020-10-17 13:15:32 +0900

    Updates

---
+++ precache-manifest.0cea637bb2094ddaec2f9bd2178d8375.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c19b3b0ad4ac8d537648f2d1519617",
+    "url": "/Algustar/index.html"
+  },
+  {
+    "revision": "a7e14963f0de9db1dc21",
+    "url": "/Algustar/static/css/main.44392440.chunk.css"
+  },
+  {
+    "revision": "296c2a8281d2a1f8d915",
+    "url": "/Algustar/static/js/2.8972a901.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Algustar/static/js/2.8972a901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7e14963f0de9db1dc21",
+    "url": "/Algustar/static/js/main.25d591cd.chunk.js"
+  },
+  {
+    "revision": "08a313e779cbcc08b160",
+    "url": "/Algustar/static/js/runtime-main.d4b4843c.js"
+  },
+  {
+    "revision": "12cb9223243b396ddc5c0d666d9bf758",
+    "url": "/Algustar/static/media/Cutar.12cb9223.png"
+  },
+  {
+    "revision": "34cd75a0ca9201fdfbbda17898513a4f",
+    "url": "/Algustar/static/media/slider1a.34cd75a0.jpg"
+  }
+]);
